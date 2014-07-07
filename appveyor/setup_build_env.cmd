a6721737bfa75e853db9808a67e9030b4d03eeca
Olivier Grisel <olivier.grisel@ensta.org>
2014-07-07 16:04:50 +0200

    tentative use of SDK v7.0

--- appveyor/setup_build_env.cmd
+++ appveyor/setup_build_env.cmd
@@ -5,10 +5,16 @@
 :: More details at:
 :: https://github.com/cython/cython/wiki/64BitCythonExtensionsOnWindows
 
-IF "%1"=="64" (
+IF "%PYTHON_ARCH%"=="64" (
     ECHO "Configuring environment to build with 64bit MSVC++"
     SET DISTUTILS_USE_SDK=1
-    "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.Cmd" /x64 /release
+    IF "%PYTHON_VERSION:~,1"=="2" (
+        ECHO "Using Windows SDK v7.0 for Python 2 compatible build"
+        "C:\Program Files\Microsoft SDKs\Windows\v7.0\Bin\SetEnv.Cmd" /x64 /release
+    ) ELSE (
+        ECHO "Using Windows SDK v7.1 for Python 3 compatible build"
+        "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.Cmd" /x64 /release
+    )
 ) ELSE (
     ECHO "Using default MSVC build environment for 32bit MSVC++"
 )
