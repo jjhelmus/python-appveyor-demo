84da2e06cb210d2a2ad3bc4f8b1d0d80667bf927
Olivier Grisel <olivier.grisel@ensta.org>
2014-07-07 16:30:51 +0200

    More debug info for version handling

--- appveyor/setup_build_env.cmd
+++ appveyor/setup_build_env.cmd
@@ -8,12 +8,17 @@
 IF "%PYTHON_ARCH%"=="64" (
     ECHO "Configuring environment to build with 64bit MSVC++"
     SET DISTUTILS_USE_SDK=1
-    IF "%PYTHON_VERSION:~,1"=="2" (
+    SET PYTHON_VERSION_MAJOR=%PYTHON_VERSION:~,1%
+    ECHO Python version: "%PYTHON_VERSION%"
+    ECHO Python major version: "%PYTHON_VERSION_MAJOR%"
+    IF "%PYTHON_VERSION_MAJOR%"=="2" (
         ECHO "Using Windows SDK v7.0 for Python 2 compatible build"
         "C:\Program Files\Microsoft SDKs\Windows\v7.0\Bin\SetEnv.Cmd" /x64 /release
-    ) ELSE (
+    ) ELSE IF "%PYTHON_VERSION_MAJOR%"=="3" (
         ECHO "Using Windows SDK v7.1 for Python 3 compatible build"
         "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.Cmd" /x64 /release
+    ) ELSE (
+        ECHO "Unexpected Python major version number: %PYTHON_VERSION_MAJOR%"
     )
 ) ELSE (
     ECHO "Using default MSVC build environment for 32bit MSVC++"
