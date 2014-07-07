7b0e5b7d81a0322913834ab1567f782a6c8a7736
Olivier Grisel <olivier.grisel@ensta.org>
2014-07-07 16:53:47 +0200

    More debugging

--- appveyor/setup_build_env.cmd
+++ appveyor/setup_build_env.cmd
@@ -6,20 +6,20 @@
 :: https://github.com/cython/cython/wiki/64BitCythonExtensionsOnWindows
 
 IF "%PYTHON_ARCH%"=="64" (
-    ECHO "Configuring environment to build with 64bit MSVC++"
+    ECHO Configuring environment to build with 64bit architecture
     SET DISTUTILS_USE_SDK=1
-    SET PYTHON_VERSION_MAJOR=%PYTHON_VERSION:~,1%
-    ECHO Python version: "%PYTHON_VERSION%"
-    ECHO Python major version: "%PYTHON_VERSION_MAJOR%"
+    SET PYTHON_VERSION_MAJOR=%PYTHON_VERSION:~0,1%
+    ECHO Python version: %PYTHON_VERSION%
+    ECHO Python major version: %PYTHON_VERSION_MAJOR%
     IF "%PYTHON_VERSION_MAJOR%"=="2" (
-        ECHO "Using Windows SDK v7.0 for Python 2 compatible build"
+        ECHO Using Windows SDK v7.0 for Python 2 compatible build
         "C:\Program Files\Microsoft SDKs\Windows\v7.0\Bin\SetEnv.Cmd" /x64 /release
     ) ELSE IF "%PYTHON_VERSION_MAJOR%"=="3" (
-        ECHO "Using Windows SDK v7.1 for Python 3 compatible build"
+        ECHO Using Windows SDK v7.1 for Python 3 compatible build
         "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.Cmd" /x64 /release
     ) ELSE (
-        ECHO "Unexpected Python major version number: %PYTHON_VERSION_MAJOR%"
+        ECHO Unexpected Python major version number: %PYTHON_VERSION_MAJOR%
     )
 ) ELSE (
-    ECHO "Using default MSVC build environment for 32bit MSVC++"
+    ECHO Using default MSVC build environment for 32bit architecture
 )
