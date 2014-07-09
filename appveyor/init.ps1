98262dc69eff5663beb7de3e1e071192af10da8e
Olivier Grisel <olivier.grisel@ensta.org>
2014-07-09 11:34:34 +0200

    debug

--- appveyor/init.ps1
+++ appveyor/init.ps1
@@ -1,6 +1,6 @@
 Write-Host $env:PYTHON $env:PYTHON_VERSION $env:PYTHON_ARCH
 ls "C:/Program Files/Microsoft SDKs/Windows"
-if ($env:PYTHON_ARCH == "64") {
+if ($env:PYTHON_ARCH = "64") {
   if ($env:PYTHON_VERSION.StartsWith("2")) {
     $env:WINDOWS_SDK_VERSION = "v7.0"
   } else {
