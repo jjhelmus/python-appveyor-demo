8265b5e633e79eac16889b07520693a731e1f23d
Olivier Grisel <olivier.grisel@ensta.org>
2014-07-09 14:38:43 +0200

    More doc

--- appveyor/select_sdk.ps1
+++ appveyor/select_sdk.ps1
@@ -1,4 +1,4 @@
-Write-Host $env:PYTHON $env:PYTHON_VERSION $env:PYTHON_ARCH
+Write-Host "Installed SDKs:"
 ls "C:/Program Files/Microsoft SDKs/Windows"
 if ($env:PYTHON_ARCH = "64") {
   if ($env:PYTHON_VERSION.StartsWith("2")) {
@@ -7,4 +7,6 @@ if ($env:PYTHON_ARCH = "64") {
     $env:WINDOWS_SDK_VERSION = "v7.1"
   }
   Write-Host "SDK" $env:WINDOWS_SDK_VERSION "for Python" $env:PYTHON_VERSION
+} else {
+  Write-Host "No SDK required for 32 bit Python builds."
 }
